ccdc733bab43e2506a40fcf704a388037c041fee
azu <azuciao@gmail.com>
2022-09-04 14:17:19 +0900

    init

---
+++ esm/index.js
@@ -0,0 +1,8 @@
+// Bundled with Packemon: https://packemon.dev
+// Platform: browser, Support: stable, Format: esm
+const some = () => {
+  return "something";
+};
+
+export { some };
+//# sourceMappingURL=index.js.map
---
+++ src/some.ts
@@ -0,0 +1,3 @@
+export const some = () => {
+  return "something"
+}
